9ce67378c94623bb9adb2e27a7deadd3978143a1
whiteduke3 <dinogit21@gmail.com>
2021-01-17 14:30:53 +0100

    Pip install requests, JSON
    Installed bs4, and urllib modules to local machine, fixed bug in urllib.request call, also data is now written to .csv as well as .json file

--- main_scraping.py
+++ main_scraping.py
@@ -1,12 +1,22 @@
 from bs4 import BeautifulSoup as bs
-from urllib.request import urlopen
+from urllib.request import urlopen, Request
 import os
 import csv
+import json
 
 if os.path.exists("C:\\Users\\Home\\PycharmProjects\\musicmaxx_scraping\\cijene.csv"):
     os.remove("C:\\Users\\Home\\PycharmProjects\\musicmaxx_scraping\\cijene.csv")
 
-page_request = urlopen("https://musicmax.si/c11545sl/c11546sl/")
+if os.path.exists(r'Prices.json'):
+    os.remove(r'Prices.json')
+
+url = "https://musicmax.si/c11545sl/c11546sl/"
+user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+headers={'User-Agent':user_agent,}
+
+
+request = Request(url,None,headers)
+page_request = urlopen(request)
 soupy = bs(page_request, "html.parser")
 pagination = soupy.find_all("ul", "pagination")[0]
 pagenum_to_search = len(pagination.find_all("li")) - 2
@@ -16,9 +26,10 @@ url_links = ["https://musicmax.si/c11545sl/c11546sl/"] + \
 
 with open("cijene.csv", mode="w", encoding="utf-8") as f:
     file_writer = csv.writer(f, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    file_writer.writerow((["IME_GITARE", "N_CIJENA(euro)", "S_CIJENA(euro)"]))
+    file_writer.writerow((["IME_GITARE", "NEW_PRICE(EUR)", "OLD_PRICE(EUR)"]))
     for url in url_links:
-        page_url = urlopen(url)
+        request1 = Request(url,None,headers)
+        page_url = urlopen(request1)
         soup = bs(page_url, "html.parser")
         for thumb in soup.find_all("div", "product-thumb"):
             for caption in thumb.find_all("div", "caption"):
@@ -27,3 +38,22 @@ with open("cijene.csv", mode="w", encoding="utf-8") as f:
                 price_new = prices.span.string
                 price_old = prices.find_all("span", "price-old")[0].string
                 file_writer.writerow(([ime_gitare, price_new[:-1], price_old[:-1]]))
+
+
+def make_json(csvFilePath, jsonFilePath): 
+    data = {} 
+    with open(csvFilePath, encoding='utf-8') as csvf: 
+        csvReader = csv.DictReader(csvf) 
+
+        for i,rows in enumerate(csvReader): 
+            key = rows['IME_GITARE'] 
+            data['GUITAR'+str(i)] = rows 
+
+    with open(jsonFilePath, 'w', encoding='utf-8') as jsonf: 
+        jsonf.write(json.dumps(data, ensure_ascii=False,indent=4))
+          
+
+csvFilePath = r'cijene.csv'
+jsonFilePath = r'Prices.json'
+  
+make_json(csvFilePath, jsonFilePath)
